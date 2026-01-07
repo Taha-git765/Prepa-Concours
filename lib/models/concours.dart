c366ed99e924cb8a4c0c236bbe4d78a6be524973
Taha-git765 <tahathiam123@gmail.com>
2026-01-07 13:16:15 +0000

    3rd commit

--- lib/models/concours.dart
+++ lib/models/concours.dart
@@ -1,1 +1,27 @@
+class Concours {
+  // Atributs
+  final String idConcours;
+  final String nomConcours;
+  final String anneeConcours;
+  final String nomEcole;
+  final String dateLimite;
+  final String dateConcours;
+  final String description;
+  final List<String> filieres;
+  final double frais;
+  final String lienInscription;
 
+  // Constructeur
+  Concours({
+    required this.idConcours,
+    required this.nomConcours,
+    required this.anneeConcours,
+    required this.nomEcole,
+    required this.dateLimite,
+    required this.dateConcours,
+    required this.description,
+    required this.filieres,
+    required this.frais,
+    required this.lienInscription,
+  });
+}
