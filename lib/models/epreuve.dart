b325a74ef7bd141b43b85fdf46f6f51a73b4bf3a
Taha-git765 <tahathiam123@gmail.com>
2026-01-08 17:51:03 +0000

    another commit

--- lib/models/epreuve.dart
+++ lib/models/epreuve.dart
@@ -1,1 +1,17 @@
+class Epreuve {
+  //Attributs
+  final String idEpreuve;
+  final String idConcours;
+  final String annee;
+  final String matiere;
+  final String nomFichier; // "esp_2024_maths.pdf"
 
+  //Constructeur
+  Epreuve({
+    required this.idEpreuve,
+    required this.idConcours,
+    required this.annee,
+    required this.matiere,
+    required this.nomFichier,
+  });
+}
